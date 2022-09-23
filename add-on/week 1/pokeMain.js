7b8c399cc25b40b9171d7ceffef79a3f5025603e
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2022-09-23 18:33:49 +0700

    change the way to handle null in most_atk function

--- add-on/week 1/pokeMain.js
+++ add-on/week 1/pokeMain.js
@@ -11,32 +11,26 @@ function fire_type(arr) {
 }
 
 function most_atk(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === null) {
-            arr.splice(i, 1);
-        }
-    }
-
     let most = [];
     let maxFire = 0;
     let maxGrass = 0;
     let maxElectric = 0;
     for (let obj of arr) {
-        if (obj.type === "fire") {
+        if (obj !== null && obj.type === "fire") {
             if (obj.atk > maxFire) {
                 maxFire = obj.atk;
                 most[0] = obj;
             } else if (obj.atk === maxFire) {
                 most.push(obj);
             } 
-        } else if (obj.type === "grass") {
+        } else if (obj !== null && obj.type === "grass") {
             if (obj.atk > maxGrass) {
                 maxGrass = obj.atk;
                 most[1] = obj;
             } else if (obj.atk === maxGrass) {
                 most.push(obj);
             }
-        } else if (obj.type === "electric") {
+        } else if (obj !== null && obj.type === "electric") {
             if (obj.atk > maxElectric) {
                 maxElectric = obj.atk;
                 most[2] = obj;
